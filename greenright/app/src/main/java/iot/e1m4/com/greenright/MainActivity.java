3f2107f1e071309f4853b8d7759db17f4f584107
ohseungo <ohseungo@gmail.com>
2018-05-23 11:23:11 +0900

    Android Project / Server Project 생성

---
+++ greenright/app/src/main/java/iot/e1m4/com/greenright/MainActivity.java
@@ -0,0 +1,14 @@
+package iot.e1m4.com.greenright;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
