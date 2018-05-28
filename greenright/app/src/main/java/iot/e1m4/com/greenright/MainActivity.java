06a2e2025255d495ca1c5303c8b55f18ae7bd28e
kys11226@gmail.com <kys11226@gmail.com>
2018-05-28 20:53:53 +0900

    view 보완 완료

--- greenright/app/src/main/java/iot/e1m4/com/greenright/MainActivity.java
+++ greenright/app/src/main/java/iot/e1m4/com/greenright/MainActivity.java
@@ -3,6 +3,7 @@ package iot.e1m4.com.greenright;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 
@@ -19,25 +20,18 @@ public class MainActivity extends AppCompatActivity {
         ActionBar actionBar=getSupportActionBar();
         actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         actionBar.setCustomView(R.layout.custom_bar);
-        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFa3c9c7));
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFffffff));
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
 
         //각 버튼 눌렀을 때 이동하는 페이지
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
             public void onTabSelected(@IdRes int tabId) {
-             switch (tabId){
-                   case R.id.tab_co2:
-                       break;
-                   case R.id.tab_cup:
-                       break;
-                 case R.id.tab_barcode:
-                     break;
-                   case R.id.tab_green_market:
-                       break;
-                   case R.id.tab_setting:
-                       break;
-               }
+
+                FragmentTransaction transaction=getSupportFragmentManager().beginTransaction();
+                if(tabId==R.id.tab_barcode){
+                    transaction.replace(R.id.contentContainer, new FragmentIntro1()).commit();
+                }
 
 
             }
