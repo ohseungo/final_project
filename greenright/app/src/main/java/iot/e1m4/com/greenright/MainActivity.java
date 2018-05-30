f8604fd50d13da6b859a58a2cfca52d2380d10cf
kys11226@gmail.com <kys11226@gmail.com>
2018-05-30 18:26:17 +0900

    view navigation menu 추가

--- greenright/app/src/main/java/iot/e1m4/com/greenright/MainActivity.java
+++ greenright/app/src/main/java/iot/e1m4/com/greenright/MainActivity.java
@@ -4,10 +4,19 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnTabSelectListener;
@@ -19,16 +28,59 @@ public class MainActivity extends AppCompatActivity {
     MapsFragment mMapsFragment = new MapsFragment();
     HomeFragment mHomeFragment = new HomeFragment();
 
+    DrawerLayout mDrawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //tool bar 관련
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ActionBar actionBar=getSupportActionBar();
-        actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
-        actionBar.setCustomView(R.layout.custom_bar);
-        actionBar.setBackgroundDrawable(new ColorDrawable(0xFFffffff));
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        //actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        //actionBar.setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        //actionBar.setCustomView(R.layout.custom_bar);
+        //actionBar.setBackgroundDrawable(new ColorDrawable(0xFFffffff));
+
         BottomBar bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+
+        //navigation view 관련
+        mDrawerLayout=findViewById(R.id.drawer_layout);
+        NavigationView navigationView=findViewById(R.id.navigationView);
+
+        //user id 입력하는 곳 = nav_header_userId
+
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                item.setChecked(true);
+                mDrawerLayout.closeDrawers();
+
+                int id=item.getItemId();
+
+                switch(id){
+                    case R.id.first_navigation_item:
+                        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.second_navigation_item:
+                        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.third_navigation_item:
+                        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return true;
+            }
+        });
 
         bottomBar.setOnTabSelectListener(new OnTabSelectListener() {
             @Override
@@ -52,6 +104,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id=item.getItemId();
+
+        switch (id){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+            case R.id.action_settings:
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onResume() {
