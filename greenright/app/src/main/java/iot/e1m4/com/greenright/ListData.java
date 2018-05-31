71590e66c922ee38ae38a08ea5b590b993dca98c
kys11226@gmail.com <kys11226@gmail.com>
2018-06-01 08:00:06 +0900

    view 포인트 내역 업데이트

---
+++ greenright/app/src/main/java/iot/e1m4/com/greenright/ListData.java
@@ -0,0 +1,23 @@
+package iot.e1m4.com.greenright;
+
+import android.graphics.drawable.Drawable;
+
+import java.text.Collator;
+import java.util.Comparator;
+
+public class ListData {
+
+    //별 아이콘
+    public Drawable mIcon;
+
+    //포인트 적립 내역
+    public String mTilte;
+
+    //적립 날짜
+    public String mDate;
+
+    //적립액
+    public String mPoint;
+
+
+}
