d8e864edfdf0e5c98888ad63de5d6765dc534a30
ohseugo@laptop <ohseungo@gmail.com>
2018-06-04 15:58:30 +0900

    merge 수정

--- server/greenRight_server/src/com/scsa/controller/WalkController.java
+++ server/greenRight_server/src/com/scsa/controller/WalkController.java
@@ -1,5 +1,7 @@
 package com.scsa.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +21,12 @@ public class WalkController {
 	@RequestMapping("/add_walk.do")
 	public @ResponseBody boolean addWalk(Walk walk) {
 		return walkService.addWalk(walk);
+	}
+	
+	@RequestMapping("/view_walk.do")
+	public @ResponseBody List<Walk> 
+			showWalkListWithUserId(String userId) {
+		return walkService.findWalkListWithUserId(userId);
 	}
 	
 	
