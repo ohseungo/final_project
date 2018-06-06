202bfd76507fb277e251d209707332aed61f167c
kys11226@gmail.com <kys11226@gmail.com>
2018-06-07 08:30:01 +0900

    서버 수정

--- greenright/app/src/main/java/info/app/AppConfig.java
+++ greenright/app/src/main/java/info/app/AppConfig.java
@@ -2,21 +2,21 @@ package info.app;
 
 public class AppConfig {
     public static final String URL_LOGIN =
-            "http://192.168.10.38:9090/greenRight_server/login.do";
+            "http://192.168.10.41:9090/greenRight_server/login.do";
     public static final String URL_REGISTER =
-            "http://192.168.10.38:9090/greenRight_server/register_user.do";
+            "http://192.168.10.41:9090/greenRight_server/register_user.do";
     public static final String FIND_USER =
-            "http://192.168.10.38:9090/greenRight_server/find_user.do";
+            "http://192.168.10.41:9090/greenRight_server/find_user.do";
 
     public static final String URL_STORE_LIST =
-            "http://192.168.10.38:9090/greenRight_server/list_store.do";
+            "http://192.168.10.41:9090/greenRight_server/list_store.do";
     public static final String URL_BOX_LIST =
-            "http://192.168.10.38:9090/greenRight_server/list_box.do";
+            "http://192.168.10.41:9090/greenRight_server/list_box.do";
 
     public static final String ADD_POINT =
-            "http://192.168.10.38:9090/greenRight_server/add_point.do";
+            "http://192.168.10.41:9090/greenRight_server/add_point.do";
     public static final String ADD_WALK =
-            "http://192.168.10.38:9090/greenRight_server/add_walk.do";
+            "http://192.168.10.41:9090/greenRight_server/add_walk.do";
 
 
 
